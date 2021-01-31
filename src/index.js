af3a21f21a5edb63d15c07187e4c32f04f1d57c5
Igor Lyatskiy <igor.lyatskiy@gmail.com>
2021-01-31 12:39:43 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (typeof matrix == "undefined" || matrix.length == 0) {
+        return [];
+    }
+    newArray = [];
+    let count = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                newArray[count] = matrix[i][j];
+                count++;
+            }
+        } else {
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                newArray[count] = matrix[i][j];
+                count++;
+            }
+        }
+    }
+    return newArray;
+};
